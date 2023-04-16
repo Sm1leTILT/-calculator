c6e0a1626fb32456d08f766e5ca8a7884d1b7d64
Sm1leTILT <128966614+Sm1leTILT@users.noreply.github.com>
2023-04-16 15:32:10 +0500

    Update cal.py

--- Cal/cal.py
+++ Cal/cal.py
@@ -1,111 +1,56 @@
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLineEdit, QGridLayout)
 
-from tkinter import *
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Calculator')
 
-# Функция для сложения двух чисел
-def add(x, y):
-    return x + y
+        self.display = QLineEdit()
+        self.display.setReadOnly(True)
 
-# Функция для вычитания двух чисел
-def subtract(x, y):
-    return x - y
+        button1 = QPushButton('0')
+        button2 = QPushButton('1')
+        button3 = QPushButton('2')
+        button4 = QPushButton('3')
+        button5 = QPushButton('4')
+        button6 = QPushButton('5')
+        button7 = QPushButton('6')
+        button8 = QPushButton('7')
+        button9 = QPushButton('8')
+        button10 = QPushButton('9')
+        button11_b = QPushButton('<-')
+        button12_C = QPushButton('C')
+        button13_p = QPushButton('+')
+        button14_m = QPushButton('-')
+        button15_u = QPushButton('*')
+        button16_d = QPushButton('/')
+        button17_r = QPushButton('=')
+        button18_p = QPushButton('.')
 
-# Функция для умножения двух чисел
-def multiply(x, y):
-    return x * y
+        layout = QGridLayout()
+        layout.addWidget(self.display, 0, 0, 1, 4)
+        layout.addWidget(button1, 5, 0)
+        layout.addWidget(button2, 4, 0)
+        layout.addWidget(button3, 4, 1)
+        layout.addWidget(button4, 4, 2)
+        layout.addWidget(button5, 3, 0)
+        layout.addWidget(button6, 3, 1)
+        layout.addWidget(button7, 3, 2)
+        layout.addWidget(button8, 2, 0)
+        layout.addWidget(button9, 2, 1)
+        layout.addWidget(button10, 2, 2)
+        layout.addWidget(button11_b, 1, 0)
+        layout.addWidget(button12_C, 1, 1)
+        layout.addWidget(button13_p, 1, 2)
+        layout.addWidget(button14_m, 1, 3)
+        layout.addWidget(button15_u, 2, 3)
+        layout.addWidget(button16_d, 3, 3)
+        layout.addWidget(button17_r, 5, 1)
+        layout.addWidget(button18_p, 5, 2)
 
-# Функция для деления двух чисел
-def divide(x, y):
-    return x / y
+        self.setLayout(layout)
 
-# Создаем экземпляр класса Tk
-root = Tk()
-root.title("Калькулятор")
-
-# Создаем поле ввода
-input_field = Entry(root, width=35, borderwidth=5)
-input_field.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
-
-# Функция для добавления цифр в поле ввода
-def button_click(number):
-    current = input_field.get()
-    input_field.delete(0, END)
-    input_field.insert(0, str(current) + str(number))
-
-# Функция для очистки поля ввода
-def button_clear():
-    input_field.delete(0, END)
-
-# Функция для выполнения операций
-def button_operation(operation):
-    global first_num
-    first_num = float(input_field.get())
-    input_field.delete(0, END)
-    global math_operation
-    math_operation = operation
-
-# Функция для вывода результата
-def button_equal():
-    second_num = float(input_field.get())
-    input_field.delete(0, END)
-
-    if math_operation == 'addition':
-        result = add(first_num, second_num)
-        input_field.insert(0, result)
-
-    elif math_operation == 'subtraction':
-        result = subtract(first_num, second_num)
-        input_field.insert(0, result)
-
-    elif math_operation == 'multiplication':
-        result = multiply(first_num, second_num)
-        input_field.insert(0, result)
-
-    elif math_operation == 'division':
-        result = divide(first_num, second_num)
-        input_field.insert(0, result)
-
-# Создаем кнопки
-button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-
-button_add = Button(root, text="+", padx=39, pady=20, command=lambda: button_operation('addition'))
-button_subtract = Button(root, text="-", padx=41, pady=20, command=lambda: button_operation('subtraction'))
-button_multiply = Button(root, text="*", padx=40, pady=20, command=lambda: button_operation('multiplication'))
-button_divide = Button(root, text="/", padx=41, pady=20, command=lambda: button_operation('division'))
-
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
-
-# Размещаем кнопки на экране
-button_1.grid(row=1, column=0)
-button_2.grid(row=1, column=1)
-button_3.grid(row=1, column=2)
-
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-
-button_7.grid(row=3, column=0)
-button_8.grid(row=3, column=1)
-button_9.grid(row=3, column=2)
-
-button_0.grid(row=4, column=0)
-
-button_add.grid(row=5, column=0)
-button_subtract.grid(row=6, column=0)
-button_multiply.grid(row=6, column=1)
-button_divide.grid(row=6, column=2)
-
-button_clear.grid(row=4, column=1, columnspan=2)
-button_equal.grid(row=5, column=1, columnspan=2)
-
-# Запускаем главный цикл
-root.mainloop()
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec_()
